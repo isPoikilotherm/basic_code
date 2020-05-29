a933f6344e12dbd92f6fb047072c32abe6426135
isPoikilotherm <18832026962@163.com>
2020-05-29 17:10:47 +0800

    ddd

---
+++ basic_code/5.29_code/src/cn/yl/ArraytoString.java
@@ -0,0 +1,10 @@
+package cn.yl;
+
+import java.util.Arrays;
+
+public class ArraytoString {
+    public static void main(String[] args) {
+        int[] array={1,2,3,4,5};
+        System.out.println(Arrays.toString(array));
+    }
+}
